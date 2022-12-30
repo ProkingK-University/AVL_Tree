b5b961c66b70e54f68a8f91b65d1199cf2bc54ca
Ashley Kapaso <ashleykapaso@gmail.com>
2022-12-30 15:11:51 +0200

    Completed impleamentation of AVL Tree

--- AvlTree.java
+++ AvlTree.java
@@ -1,3 +1,5 @@
+import javax.lang.model.util.ElementScanner14;
+
 public class AvlTree<T extends Comparable<T>>
 {
     public Node<T> root;
@@ -5,16 +7,6 @@ public class AvlTree<T extends Comparable<T>>
     public AvlTree()
     {
         this.root = null;
-    }
-
-    int getHeight(Node<T> N)
-    {
-        if (N == null)
-        {
-            return 0;
-        }
-
-        return N.height;
     }
 
     void print(Node<T> node)
@@ -47,9 +39,38 @@ public class AvlTree<T extends Comparable<T>>
             {
                 node.right = insert(node.right, data);
             }
-
-            return node;
+            else
+            {
+                return root;
+            }
         }
+
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+
+        int balanceFactor = getBalanceFactor(node);
+
+        if (balanceFactor > 1 && data.compareTo(node.left.data) < 0)
+        {
+            return rightRotate(node);
+        }
+        else if (balanceFactor < -1 && data.compareTo(node.right.data) > 0)
+        {
+            return leftRotate(node);
+        }
+        else if (balanceFactor > 1 && data.compareTo(node.left.data) > 0)
+        {
+            node.left = leftRotate(node.left);
+
+            return rightRotate(node);
+        }
+        else if (balanceFactor < -1 && data.compareTo(node.right.data) < 0)
+        {
+            node.right = rightRotate(node.right);
+
+            return leftRotate(node);
+        }
+
+        return node;
     }
 
 
@@ -62,7 +83,7 @@ public class AvlTree<T extends Comparable<T>>
     {
         if (node == null)
         {
-            return null;
+            return node;
         }
         else if (data.compareTo(node.data) < 0)
         {
@@ -90,7 +111,40 @@ public class AvlTree<T extends Comparable<T>>
             }
         }
 
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+
+        int balanceFactor = getBalanceFactor(node);
+
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+        {
+            return rightRotate(node);
+        }
+        else if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0)
+        {
+            return leftRotate(node);
+        }
+        else if (balanceFactor > 1 && getBalanceFactor(node.left) < 0)
+        {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        else if (balanceFactor < -1 && getBalanceFactor(node.right) > 0)
+        {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
         return node;
+    }
+
+    int getHeight(Node<T> N)
+    {
+        if (N == null)
+        {
+            return 0;
+        }
+
+        return N.height;
     }
 
     private Node<T> findMin(Node<T> node)
@@ -99,7 +153,45 @@ public class AvlTree<T extends Comparable<T>>
         {
             node = node.left;
         }
-        
+
         return node;
     }
+
+    private int getBalanceFactor(Node<T> node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    private Node<T> rightRotate(Node<T> node)
+    {
+        Node<T> leftNode = node.left;
+        Node<T> rightNode = leftNode.right;
+
+        leftNode.right = node;
+        node.left = rightNode;
+
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        leftNode.height = 1 + Math.max(getHeight(leftNode.left), getHeight(leftNode.right));
+
+        return leftNode;
+    }
+
+    private Node<T> leftRotate(Node<T> node)
+    {
+        Node<T> rightNode = node.right;
+        Node<T> leftNode = rightNode.left;
+
+        rightNode.left = node;
+        node.right = leftNode;
+
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        rightNode.height = 1 + Math.max(getHeight(rightNode.left), getHeight(rightNode.right));
+
+        return rightNode;
+    }   
 }
